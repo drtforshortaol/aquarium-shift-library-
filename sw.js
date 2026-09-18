890b27c759a8d16528dbda267d4ff7cfb74fb22c
Glenn Takenaga DDS. MSc <drtforshort@aol.com>
2026-09-18 08:26:02 +0300

    Refresh offline cache for library filter correction

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const C='aquarium-library-v1';const A=['./','./index.html','./library-data.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='aquarium-library-v2';const A=['./','./index.html','./library-data.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request,{cacheName:C}).then(r=>r||fetch(e.request))));
